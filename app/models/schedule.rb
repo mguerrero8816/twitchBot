d15649d65608b4f62c3bb5512c56adb9c6c8800d
Mike Guerrero <mguerrero@ceatus.com>
2017-12-05 09:13:45 -0800

    removed unneeded logic

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,11 +1,3 @@
 class Schedule < ApplicationRecord
   attr_accessor :start_at_date, :start_at_time
-
-  def start_at_date
-    Date.new(start_at.year, start_at.month, start_at.day) if start_at
-  end
-
-  def start_at_time
-    start_at
-  end
 end
