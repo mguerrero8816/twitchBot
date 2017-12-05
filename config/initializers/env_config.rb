7e765001c8f078a31917949d964d4cfd58ff769b
Mike Guerrero <mguerrero@ceatus.com>
2017-12-05 11:28:18 -0800

    removed env loader

--- config/initializers/env_config.rb
+++
@@ -1,3 +0,0 @@
-ENVIRONMENTS_CONFIG = YAML.load(File.read("#{Rails.root}/config/environments.yml")) rescue {}
-ENV['foo'] ||= ENVIRONMENTS_CONFIG['foo']
-ENV['secret_key_base'] ||= ENVIRONMENTS_CONFIG['secret_key_base']
