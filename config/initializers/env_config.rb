064f9764651f87c378aa903dce944624b1fc0933
Mike Guerrero <mguerrero@ceatus.com>
2017-12-05 11:12:43 -0800

    removed secrets from gitignore

--- config/initializers/env_config.rb
+++ config/initializers/env_config.rb
@@ -1,2 +1,3 @@
 ENVIRONMENTS_CONFIG = YAML.load(File.read("#{Rails.root}/config/environments.yml"))
 ENV['foo'] ||= ENVIRONMENTS_CONFIG['foo']
+ENV['secret_key_base'] ||= ENVIRONMENTS_CONFIG['secret_key_base']
