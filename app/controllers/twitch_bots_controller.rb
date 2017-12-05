89ba470c421d19c7afc7f49fbb76604428344dfb
Mike Guerrero <mguerrero@ceatus.com>
2017-12-04 21:25:10 -0800

    schedules link to home

--- app/controllers/twitch_bots_controller.rb
+++ app/controllers/twitch_bots_controller.rb
@@ -2,9 +2,11 @@ class TwitchBotsController < ApplicationController
 
   def connect
     TwitchConnector.connect
+    redirect_to root_path
   end
 
   def disconnect
     TwitchConnector.disconnect
+    redirect_to root_path
   end
 end
