2f04df64371343e7e452f07498f1504b3adf716b
Mike Guerrero <mguerrero@ceatus.com>
2017-12-05 09:17:28 -0800

    formatted date and time columns

--- app/helpers/table_format_helper.rb
+++ app/helpers/table_format_helper.rb
@@ -2,4 +2,8 @@ module TableFormatHelper
   def standard_table_date(date)
     date.strftime('%m/%d/%y') if date
   end
+
+  def standard_table_time(time)
+    time.strftime('%I:%M%P') if time
+  end
 end
