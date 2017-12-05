63c9942da5ed5fa0ab8ab5b5a13d8a9f69953e47
Mike Guerrero <mguerrero@ceatus.com>
2017-12-05 11:25:45 -0800

    rescuing no file

--- config/initializers/env_config.rb
+++ config/initializers/env_config.rb
@@ -1,3 +1,3 @@
-ENVIRONMENTS_CONFIG = YAML.load(File.read("#{Rails.root}/config/environments.yml"))
+ENVIRONMENTS_CONFIG = YAML.load(File.read("#{Rails.root}/config/environments.yml")) rescue {}
 ENV['foo'] ||= ENVIRONMENTS_CONFIG['foo']
 ENV['secret_key_base'] ||= ENVIRONMENTS_CONFIG['secret_key_base']
