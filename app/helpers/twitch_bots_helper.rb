2523c9bc414cd65d56563f498039c8a5fd71f132
Mike Guerrero <mguerrero@ceatus.com>
2017-12-04 21:42:36 -0800

    added bot status

---
+++ app/helpers/twitch_bots_helper.rb
@@ -0,0 +1,7 @@
+module TwitchBotsHelper
+  def bot_status(thread_name)
+    twitch_bot_threads = Thread.list.select{|thread| thread[:name] == thread_name && thread.status == 'sleep' }
+    bot_active = twitch_bot_threads.count > 0
+    bot_active ? 'Active' : 'Inactive'
+  end
+end
