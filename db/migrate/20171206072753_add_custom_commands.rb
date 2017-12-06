e87027c5a9451ad7ddb1a90434fc088a0f6470ea
Mike Guerrero <mguerrero@ceatus.com>
2017-12-05 23:28:50 -0800

    added and ran migration for custom commands

---
+++ db/migrate/20171206072753_add_custom_commands.rb
@@ -0,0 +1,14 @@
+class AddCustomCommands < ActiveRecord::Migration[5.0]
+  def up
+    create_table :custom_commands do |t|
+      t.string :command
+      t.string :response
+
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :schedules
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171205031732) do
+ActiveRecord::Schema.define(version: 20171206072753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "custom_commands", force: :cascade do |t|
+    t.string   "command"
+    t.string   "response"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "schedules", force: :cascade do |t|
     t.string   "title"
