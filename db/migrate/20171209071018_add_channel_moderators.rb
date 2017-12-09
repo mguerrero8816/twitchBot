6bc8c8abe4663f228136ee86a03a2f6c249b19dd
Mike Guerrero <mguerrero@ceatus.com>
2017-12-08 23:23:32 -0800

    added and ran channel moderators migration

---
+++ db/migrate/20171209071018_add_channel_moderators.rb
@@ -0,0 +1,14 @@
+class AddChannelModerators < ActiveRecord::Migration[5.0]
+  def up
+    create_table :channel_moderators do |t|
+      t.string :moderator_name
+      t.string :channel_name
+
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :schedules
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171207040545) do
+ActiveRecord::Schema.define(version: 20171209071018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20171207040545) do
     t.string   "bot_name"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+  end
+
+  create_table "channel_moderators", force: :cascade do |t|
+    t.string   "moderator_name"
+    t.string   "channel_name"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   create_table "custom_commands", force: :cascade do |t|
