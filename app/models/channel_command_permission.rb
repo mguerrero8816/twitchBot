8e75041c3d38b5b22ddcc8457a44c5ded5f2d5af
Mike Guerrero <mguerrero@ceatus.com>
2017-12-09 21:25:52 -0800

    added channel command permission validations

--- app/models/channel_command_permission.rb
+++ app/models/channel_command_permission.rb
@@ -1,5 +1,7 @@
 class ChannelCommandPermission < ApplicationRecord
   PERMISSIONS = [ 'Admins', 'All' ].freeze
+  validates_uniqueness_of :custom_command_id, scope: :channel_bot_id
+
 
   def permission_name
     PERMISSIONS[permission_id]
