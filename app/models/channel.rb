cd66a359a91da164e289a02a0569a3d3426aa851
Mike Guerrero <mguerrero@ceatus.com>
2017-12-13 00:03:58 -0800

    able to log in with channel name

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -4,6 +4,13 @@ class Channel < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  after_create :update_email
+  validates_uniqueness_of :name, case_sensitive: false
+
+  def update_email
+    self.update_attribute('email', "#{name}@test.com")
+  end
+
   def email_required?
     false
   end
