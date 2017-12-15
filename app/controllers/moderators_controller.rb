d5dfb33274677970a10aeca7921b93e9eeced703
Mike Guerrero <mguerrero@ceatus.com>
2017-12-14 23:31:18 -0800

    able to create moderator

--- app/controllers/moderators_controller.rb
+++ app/controllers/moderators_controller.rb
@@ -4,16 +4,24 @@ class ModeratorsController < ApplicationController
   end
 
   def create
+    @moderator = Moderator.new(moderator_params)
+    if @moderator.save
+    else
+    end
+    redirect_to moderators_path
   end
 
   def update
+    redirect_to moderators_path
   end
 
   def destroy
+    redirect_to moderators_path
   end
 
   private
 
   def moderator_params
+    params.require(:moderator).permit(:name, :channel_id)
   end
 end
--- app/models/moderator.rb
+++ app/models/moderator.rb
@@ -1,3 +1,4 @@
 class Moderator < ApplicationRecord
   validates_uniqueness_of :name, scope: %i[channel_id], case_sensitive: false
+  validates :name, :channel_id, presence: true
 end
