58f07b5b5fd4d33c1d353ab34a6a21923678b023
Mike Guerrero <mguerrero@ceatus.com>
2017-12-14 20:39:08 -0800

    edit form js working

--- app/helpers/filters_helper.rb
+++ app/helpers/filters_helper.rb
@@ -4,8 +4,8 @@ module FiltersHelper
     options_for_select(custom_commands.map{|command| ["!#{command.command}", command.id]})
   end
 
-  def permission_options_for_select
+  def permission_options_for_select(selected=nil)
     permissions = CommandPermission::PERMISSIONS
-    options_for_select(permissions.map.with_index{|permission, index| [permission, index]})
+    options_for_select(permissions.map.with_index{|permission, index| [permission, index]}, selected: selected)
   end
 end
