a722f18356effa48c17ac89d32441a494a4651f2
Mike Guerrero <mguerrero@ceatus.com>
2017-12-15 23:44:03 -0800

    added commands index

--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,18 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
 
-  resource :twitch_bots, only: [] do
-    post :connect
-    post :disconnect
-  end
-
-  resources :schedules
-  resources :custom_commands, except: [:new, :edit]
-  resources :moderators, except: [:new, :edit]
   resources :channel_bots do
     post   :add_moderator,              on: :member
     post   :add_command_permission,     on: :member
     delete :destroy_moderator,          on: :member
     delete :destroy_command_permission, on: :member
+  end
+  resources :custom_commands, except: [:new, :edit]
+  resources :commands
+  resources :moderators, except: [:new, :edit]
+  resources :schedules
+  resource :twitch_bots, only: [] do
+    post :connect
+    post :disconnect
   end
 end
