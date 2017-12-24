d7e9266db3d4e3e66b33bf771d9eee2154db8999
Mike Guerrero <mguerrero@ceatus.com>
2017-12-23 20:26:23 -0800

    added form for defined command permissions

--- app/controllers/commands_controller.rb
+++ app/controllers/commands_controller.rb
@@ -4,7 +4,7 @@ class CommandsController < ApplicationController
     command_sql_data = CommandPermission.where('command_permissions.channel_id = ? AND command_permissions.command_name IN (?) AND command_permissions.command_id IS NULL', current_channel.id, command_names)
     commands_data = {}
     command_sql_data.each do |sql_data|
-      commands_data[sql_data.command_name] = sql_data
+      commands_data[sql_data.command_name.to_sym] = sql_data
     end
     Struct.new('BotCommand', :id, :name, :permission_id, :channel_id)
     @commands = []
@@ -13,5 +13,18 @@ class CommandsController < ApplicationController
       bot_command = Struct::BotCommand.new(sql_data.try(:id), command_name, sql_data.try(:permission_id), sql_data.try(:channel_id))
       @commands << bot_command
     end
+  end
+
+  def update
+    if params[:id] == '0'
+      permission = CommandPermission.new(command_name: params[:command][:name], channel_id: params[:command][:channel_id])
+    else
+      permission = CommandPermission.find(params[:id])
+    end
+    permission.permission_id = params[:command][:permission_id]
+    if permission.save
+    else
+    end
+    redirect_to commands_path
   end
 end
