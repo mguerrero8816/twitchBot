50ea287b26006d934602647d6a9cfdddf67437b1
Mike Guerrero <mguerrero@ceatus.com>
2017-12-28 21:13:52 -0800

    able to edit custom command repeater settings

--- app/controllers/custom_commands_controller.rb
+++ app/controllers/custom_commands_controller.rb
@@ -2,9 +2,12 @@ class CustomCommandsController < ApplicationController
   def index
     @custom_commands = (
     CustomCommand.select('custom_commands.*,
-                          COALESCE(command_permissions.permission_id, 0) AS command_permission_id')
-                 .where('custom_commands.channel_id = ?', current_channel.id)
+                          COALESCE(command_permissions.permission_id, 0) AS command_permission_id,
+                          command_repeaters.start_at AS repeater_start_at,
+                          command_repeaters.cycle_second AS repeater_cycle_time')
                  .joins('LEFT JOIN command_permissions ON custom_commands.id = command_permissions.command_id')
+                 .joins('LEFT JOIN command_repeaters ON custom_commands.id = command_repeaters.command_id')
+                 .where('custom_commands.channel_id = ?', current_channel.id)
                  .order('LOWER(command) ASC')
     )
   end
@@ -33,6 +36,6 @@ class CustomCommandsController < ApplicationController
   private
 
   def custom_command_params
-    params.require(:custom_command).permit(:command, :response, :channel_id, :permission_id)
+    params.require(:custom_command).permit(:command, :response, :channel_id, :permission_id, :repeater_start, :repeater_cycle)
   end
 end
--- app/models/custom_command.rb
+++ app/models/custom_command.rb
@@ -1,8 +1,8 @@
 class CustomCommand < ApplicationRecord
   validates :command, format: { with: /\A[A-Za-z0-9]+\z/ }, uniqueness: { case_sensitive: false, scope: :channel_id }
   validates :command, :response, :channel_id, presence: true
-  attr_accessor :permission_id
-  after_commit :update_permission
+  attr_accessor :permission_id, :repeater_start, :repeater_cycle
+  after_commit :update_permission, :update_repeater
 
   private
 
@@ -12,5 +12,12 @@ class CustomCommand < ApplicationRecord
       current_permission.permission_id = permission_id
       current_permission.save
     end
+  end
+
+  def update_repeater
+    current_repeater = CommandRepeater.where(command_id: id).first || CommandRepeater.new(command_id: id, channel_id: channel_id)
+    current_repeater.start_at = repeater_start
+    current_repeater.cycle_second = repeater_cycle
+    current_repeater.save
   end
 end
