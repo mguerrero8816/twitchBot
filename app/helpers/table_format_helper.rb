db5bcd8feb7ff262dfb78fd73cce2342f43dd582
Mike Guerrero <mguerrero@ceatus.com>
2018-01-01 22:58:47 -0800

    reformatted standard table methods

--- app/helpers/table_format_helper.rb
+++ app/helpers/table_format_helper.rb
@@ -1,9 +1,13 @@
 module TableFormatHelper
   def standard_table_date(date)
-    date.strftime('%m/%d/%y') if date
+    date ? date.strftime('%m/%d/%y') : '-'
   end
 
   def standard_table_time(time)
-    time.strftime('%I:%M%P') if time
+    time ? time.strftime('%I:%M%P') : '-'
+  end
+
+  def standard_table_text(string)
+    string.blank? ? '-' : string
   end
 end
